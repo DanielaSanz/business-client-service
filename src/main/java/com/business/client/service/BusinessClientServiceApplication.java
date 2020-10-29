2e34fe8e40315557f773789c473ce2a468fbceec
Daniela Sanchez <47358877+reinamarla@users.noreply.github.com>
2020-10-29 16:28:56 -0300

    Initial commit

---
+++ src/main/java/com/business/client/service/BusinessClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.business.client.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessClientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessClientServiceApplication.class, args);
+    }
+
+}
